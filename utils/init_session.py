fea00c9e709b4045ea420238390ed699dd3abd78
szeyu <szeyusim@gmail.com>
2024-10-19 17:30:14 +0800

    Refactor session initialization to remove unused variables and add file path to session state

--- utils/init_session.py
+++ utils/init_session.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import os
-from jamaibase import JamAI
-import dotenv
-dotenv.load_dotenv()
-
-JAMAIBASE_API_KEY = os.getenv("JAMAIBASE_API_KEY")
-PROJ_ID = os.getenv("PROJ_ID")
 
 def init_session():
     if "authenticated" not in st.session_state:
@@ -18,9 +11,5 @@ def init_session():
         st.session_state["chat_history"] = []
     if "system_prompt" not in st.session_state:
         st.session_state["system_prompt"] = "You are helpful assistant"
-    if "knowledge_base_id" not in st.session_state:
-        st.session_state["knowledge_base_id"] = "tutor-1-algebra"
-        
-    # Initialize JamAI client
-    if "jamai_client" not in st.session_state:
-        st.session_state.jamai_client = JamAI(api_key=JAMAIBASE_API_KEY, project_id=PROJ_ID)
+    if "file_path" not in st.session_state:
+        st.session_state["file_path"] = ""
